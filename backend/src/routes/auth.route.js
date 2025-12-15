6ecf30be7be0ade446fbd4d5dfb66a984389af4d
ManishYadav7982 <manishya12390@gmail.com>
2025-12-15 22:42:07 +0530

    chat app git init

---
+++ backend/src/routes/auth.route.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const router = express.Router();
+
+// Define your authentication routes here
+
+router.get("/login", (req, res) => {
+  res.send("Login route");
+});
+
+router.get("/register", (req, res) => {
+  res.send("Register route");
+}); 
+
+router.get("/logout", (req, res) => {
+  res.send("Logout route");
+});
+
+
+export default router;
